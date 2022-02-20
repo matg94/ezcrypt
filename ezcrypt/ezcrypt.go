440ae53421ad92053ab793bda3031aa74a2ca363
mat <mat.gautron@gmail.com>
2022-02-20 10:08:57 +0000

    Added encryption, decryption and tests

--- ezcrypt/ezcrypt.go
+++ ezcrypt/ezcrypt.go
@@ -1,15 +1,82 @@
 package ezcrypt
 
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
 func VerifySignature() {}
 
 func GenerateSignature() {}
-
-func EncryptFile() {}
 
 func GenerateSignatureToFile() {}
 
 func VerifyFileSignature() {}
 
-func Encrypt() {}
+func EncryptFile(publicKey *rsa.PublicKey, filepath string) error {
+	plainTextFile, err := os.ReadFile(filepath)
+	if err != nil {
+		return err
+	}
 
-func Decrypt() {}
+	cipherTextFile, err := Encrypt(publicKey, string(plainTextFile))
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(filepath+".ezcrypt", []byte(cipherTextFile), 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DecryptFile(privateKey *rsa.PrivateKey, filepath string) error {
+	cipherTextFile, err := os.ReadFile(filepath)
+	if err != nil {
+		return err
+	}
+
+	plainTextFile, err := Decrypt(privateKey, string(cipherTextFile))
+	if err != nil {
+		return err
+	}
+	filePathSplit := strings.Split(filepath, ".")
+	newFilePath := strings.Join(filePathSplit[:len(filePathSplit)-1], ".")
+	err = ioutil.WriteFile(newFilePath, []byte(plainTextFile), 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Encrypt(publicKey *rsa.PublicKey, plaintext string) (string, error) {
+	label := []byte("OAEP Encrypted")
+	rng := rand.Reader
+	ciphertext, err := rsa.EncryptOAEP(sha256.New(), rng, publicKey, []byte(plaintext), label)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
+
+func Decrypt(privateKey *rsa.PrivateKey, cipher string) (string, error) {
+	ciphertext, err := base64.StdEncoding.DecodeString(cipher)
+	if err != nil {
+		fmt.Println("Failed to base64 decode", err)
+	}
+
+	label := []byte("OAEP Encrypted")
+	rng := rand.Reader
+
+	plaintext, err := rsa.DecryptOAEP(sha256.New(), rng, privateKey, ciphertext, label)
+	if err != nil {
+		return "", err
+	}
+	return string(plaintext), nil
+}
--- ezcrypt/ezcrypt_test.go
+++ ezcrypt/ezcrypt_test.go
@@ -1,17 +1,89 @@
 package ezcrypt
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
 
 func TestVerifySignature(t *testing.T) {}
 
 func TestGenerateSignature(t *testing.T) {}
 
-func TestEncryptFile(t *testing.T) {}
-
 func TestGenerateSignatureToFile(t *testing.T) {}
 
 func TestVerifyFileSignature(t *testing.T) {}
 
-func TestEncrypt(t *testing.T) {}
+func TestEncryptAndDecryptFile(t *testing.T) {
+	privateKey, _ := GeneratePrivateKey()
+	publicKey := GetPublicKey(privateKey)
+	testEncryptionContent := "test-encryption-content"
 
-func TestDecrypt(t *testing.T) {}
+	ioutil.WriteFile("./testFile.txt", []byte(testEncryptionContent), 0644)
+
+	err := EncryptFile(publicKey, "./testFile.txt")
+	if err != nil {
+		t.Logf("expected encrypt file err to be <nil> but got %v", err)
+		t.Fail()
+	}
+
+	cipherTextFile, err := os.ReadFile("./testFile.txt.ezcrypt")
+
+	if err != nil {
+		t.Logf("expected reading file err to be <nil> but got %v", err)
+		t.Fail()
+	}
+
+	if string(cipherTextFile) == testEncryptionContent {
+		t.Log("expected cipher and plaintext to be different")
+		t.Fail()
+	}
+
+	cleanUpFile(t, "./testFile.txt")
+
+	err = DecryptFile(privateKey, "./testFile.txt.ezcrypt")
+	if err != nil {
+		t.Logf("expected decrypt file err to be <nil> but got %v", err)
+		t.Fail()
+	}
+	decryptedPlainTextFile, err := os.ReadFile("./testFile.txt")
+	if err != nil {
+		t.Logf("expected reading decrypted file err to be <nil> but got %v", err)
+		t.Fail()
+	}
+
+	if string(decryptedPlainTextFile) != testEncryptionContent {
+		t.Log("expected decrypted content to match original")
+		t.Fail()
+	}
+
+	cleanUpFile(t, "./testFile.txt.ezcrypt")
+	cleanUpFile(t, "./testFile.txt")
+
+}
+
+func TestEncryptAndDecrypt(t *testing.T) {
+	privateKey, _ := GeneratePrivateKey()
+	publicKey := GetPublicKey(privateKey)
+
+	testPlainText := "test-encryption-message"
+
+	cipher, err := Encrypt(publicKey, testPlainText)
+	if err != nil {
+		t.Logf("expected encryption err to be <nil> but got %v", err)
+		t.Fail()
+	}
+	if cipher == testPlainText {
+		t.Logf("expected cipher to differ from plaintext but they matched")
+		t.Fail()
+	}
+	plainTextDecrypted, err := Decrypt(privateKey, cipher)
+	if err != nil {
+		t.Logf("expected decryption err to be <nil> but got %v", err)
+		t.Fail()
+	}
+	if plainTextDecrypted != testPlainText {
+		t.Logf("expected decryption to return original plaintext but they did not match got: %s != %s", plainTextDecrypted, testPlainText)
+		t.Fail()
+	}
+}
